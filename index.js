898bd4a4c89e2b6c2a788fdd1d590196fb94ca92
darreniyer0601 <54214698+darreniyer0601@users.noreply.github.com>
2019-10-12 09:27:00 -0400

    Sign in page
    Created firebase authorisation code and a database to store sign in events. Created the page to handle the sign ins.

---
+++ index.js
@@ -0,0 +1,60 @@
+(function() {
+
+    //Initializing Firebase
+    const config = {
+        apiKey: "AIzaSyAb0lZGuafGwHIfgOmdm-oHj90X1ifsUSo",
+        authDomain: "mhacks-project-15030.firebaseapp.com",
+        databaseURL: "https://console.firebase.google.com/project/mhacks-project-15030/database/firestore/data~2F",
+        storageBucket: "mhacks-project-15030",
+
+    };
+    firebase.initializeApp(config);
+    
+    //Get elements
+    const txtEmail = document.getElementById('txtEmail');
+    const txtPassword = document.getElementById('txtPassword');
+    const btnLogin = document.getElementById('btnLogin');
+    const btnSignup = document.getElementById('btnSignUp');
+    const btnLogout = document.getElementById('btnLogout');
+
+    //Add login event
+    btnLogin.addEventListener('click',e => {
+        // Get email and pass
+        const email = txtEmail.value;
+        const pass = txtPassword.value;
+        const auth = firebase.auth();
+
+        // Sign in
+        const promise = auth.signInWithEmailAbdPassword(email,
+            pass);
+            promise.catch(e => console.log(e.getMessage));
+        });
+
+    // Add signup event
+    btnLogin.addEventListener('click',e => {
+        // Get email and pass
+        const email = txtEmail.value;
+        const pass = txtPassword.value;
+        const auth = firebase.auth();
+
+        // Sign in
+        const promise = auth.createUserWithEmailAbdPassword(email,
+            pass);
+            promise.catch(e => console.log(e.getMessage));
+    })
+
+    btnLogout.addEventListener('click', e=>{
+        firebase.auth.signOut();
+    });
+
+    // Add a realtime listener
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+        if(firebaseUser) {
+            console.log(firebaseUser);
+        } else {
+            console.log('not logged in');
+            btnLogout.classList.add('hide');
+        }
+    })
+
+}); 
